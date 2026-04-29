9236e718107ce7b4cd6fe795774780bfd17ab6bd
graff-mc911 <125906318+graff-mc911@users.noreply.github.com>
2026-04-29 05:18:03 +0200

    Updated index.ts

--- src/lib/locales/index.ts
+++ src/lib/locales/index.ts
@@ -1,16 +1,1 @@
-// locales/index.ts
-
-import { enTranslations } from "../Translations/en";
-import { ukTranslations } from "../Translations/uk";
-
-export const translations = {
-  en: enTranslations,
-  uk: ukTranslations,
-} as const;
-
-export type LanguageCode = keyof typeof translations;
-export type TranslationKey = keyof typeof enTranslations;
-
-export function getTranslation(lang: LanguageCode, key: TranslationKey) {
-  return translations[lang]?.[key] || translations.en[key] || key;
-}
+export { translations, getTranslation } from '../i18n'
