fc90b9656d2826f39e70cf37733c29e228fc2d80
graff-mc911 <125906318+graff-mc911@users.noreply.github.com>
2026-05-02 20:59:07 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Ставимо відносну base-адресу,
+// щоб іконки, assets і built-файли працювали не лише з кореня домену.
 export default defineConfig({
+  base: './',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
